8b461b47de5018721acec55bc7ac2225290eb817
nmkha-gitlab <nguyenminhkha2001@gmail.com>
2022-10-25 01:09:21 +0700

    re structure project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from './pages/HomePage/HomePage';
+import WorkPage from './pages/WorkPage/WorkPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />}/>
+        <Route path="home" element={<HomePage />} />
+        <Route path="work" element={<WorkPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/modules/example/component/ExampleComponent2/index.ts
@@ -0,0 +1,3 @@
+import ExampleComponent1 from "./ExampleComponent1.component";
+
+export default ExampleComponent1;
