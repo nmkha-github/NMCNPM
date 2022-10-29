59a72c1d60b79ccf9805646a092983228550030b
QuangHuy54 <73436308+QuangHuy54@users.noreply.github.com>
2022-10-29 22:56:35 +0700

    CNPM24-16 Fix some UI for login form

--- src/modules/home/components/LoginForm/LoginForm.tsx
+++ src/modules/home/components/LoginForm/LoginForm.tsx
@@ -8,8 +8,8 @@ const LoginForm = () => {
   return (
     <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="w-full max-w-md space-y-8">
-        <div></div>
-        <div className="bg-white md:shadow-2xl rounded px-8 pt-6 pb-8 mb-4 flex flex-col mt-8 space-y-6">
+        <div className="bg-white lg:shadow-2xl rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
+          <div className="mt-8 space-y-6">
           <h2 className="mt-6 text-left text-3xl font-medium tracking-tight text-gray-900">
             Sign in
           </h2>
@@ -37,7 +37,7 @@ const LoginForm = () => {
                 id="remember-me"
                 name="remember-me"
                 type="checkbox"
-                className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                className="h-4 w-4 rounded border-gray-300"
               />
               <label
                 htmlFor="remember-me"
@@ -50,25 +50,26 @@ const LoginForm = () => {
           <div>
             <Button
               id="submitButton"
-              className="flex w-full justify-center border border-transparent py-2 px-4 font-normal text-white focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              className="flex w-full justify-center border border-transparent px-4  focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
               onClick={() => {
                 console.log(account);
                 console.log(password);
               }}
             >
-              <Typography style={{ color: "white" }}>Sign in</Typography>
+              Sign in
             </Button>
           </div>
-          <div id="seperator_login_register"></div>
-          <div className="flex items-center justify-center">
-            <a
-              href="/register"
-              id="createNewAccountButton"
-              className="btn flex w-3/5 justify-center rounded-3xl border-4 border-solid py-2 px-4 text-xl font-bold font-normal"
-            >
-              Create New Account
-            </a>
           </div>
+          <div id="seperator_login_register" className="flex my-4 items-center justify-center"><span className="mx-3">or</span></div>
+          <div className="flex items-center justify-center">
+              <a
+                  href="/register"
+                  id="createNewAccountButton"
+                  className="btn flex w-4/5 justify-center rounded-3xl border-4 border-solid py-2 px-4 text-xl font-bold font-normal text-center"
+                >
+                  Create New Account
+                </a>
+            </div>
         </div>
       </div>
     </div>
