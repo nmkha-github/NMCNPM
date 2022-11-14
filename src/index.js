e3422e51b8209866c91f4a6499c689613559e765
nmkha-gitlab <nguyenminhkha2001@gmail.com>
2022-11-14 23:43:46 +0700

    CNPM24-41 update room provider

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AppSnackbarProvider from './lib/provider/AppSnackBarProvider';
 import HomePage from './pages/HomePage/HomePage';
 import WorkPage from './pages/WorkPage/WorkPage';
 import RoomPage from './pages/RoomPage/RoomPage';
+import RoomsProvider from './modules/home/provider/RoomsProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />}/>
-        <Route path="home" element={<HomePage />} />
-        <Route path="work" element={<WorkPage />} />
-        <Route path="room" element={<RoomPage />} />
-      </Routes>
+      <AppSnackbarProvider>
+        <RoomsProvider>
+          <Routes>
+            <Route path="/" element={<HomePage />}/>
+            <Route path="home" element={<HomePage />} />
+            <Route path="work" element={<WorkPage />} />
+            <Route path="room" element={<RoomPage />} />
+          </Routes>
+        </RoomsProvider>
+      </AppSnackbarProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/modules/home/provider/RoomsProvider.tsx
@@ -0,0 +1,110 @@
+import React, { createContext, useCallback, useContext, useState } from "react";
+import useAppSnackbar from "../../../lib/hook/useAppSnackBar";
+import RoomData from "../interface/room-data";
+
+interface RoomsContextProps {
+  rooms: RoomData[];
+
+  getRooms: (payload: { limit?: number; skip?: number }) => void;
+  loadingRooms: boolean;
+  loadingMoreRooms: boolean;
+
+  updateRoom: (payload: { updateRoom: RoomData }) => void;
+  updatingRoom: boolean;
+}
+
+const RoomsContext = createContext<RoomsContextProps>({
+  rooms: [],
+
+  getRooms: () => {},
+  loadingRooms: false,
+  loadingMoreRooms: false,
+
+  updateRoom: () => {},
+  updatingRoom: false,
+});
+
+interface TestContextProviderProps {
+  children: React.ReactNode;
+}
+
+const RoomsProvider = ({ children }: TestContextProviderProps) => {
+  const [rooms, setRooms] = useState<RoomData[]>([]);
+  const [loadingRooms, setLoadingRooms] = useState(false);
+  const [loadingMoreRooms, setLoadingMoreRooms] = useState(false);
+  const [updatingRoom, setUpdatingRoom] = useState(false);
+
+  const { showSnackbarError } = useAppSnackbar();
+
+  const getRooms = useCallback(
+    async ({ limit, skip }: { limit?: number; skip?: number }) => {
+      setLoadingRooms(true);
+      try {
+        setRooms([
+          ...rooms,
+          ...Array(6)
+            .fill(1)
+            .map((_, index) => {
+              return {
+                id: String(rooms.length + index + 1),
+                name: `Phòng ${rooms.length + index + 1}`,
+                description: `Description ${rooms.length + index + 1}`,
+              };
+            }),
+        ]);
+      } catch (error) {
+        showSnackbarError(error);
+      } finally {
+        setLoadingRooms(false);
+      }
+    },
+    [rooms, showSnackbarError]
+  );
+
+  const updateRoom = useCallback(
+    async ({ updateRoom }: { updateRoom: RoomData }) => {
+      setUpdatingRoom(true);
+      try {
+        if (updateRoom.description) {
+          rooms.map((room) => {
+            if (room.id === updateRoom.id) {
+              return {
+                ...room,
+                description: updateRoom.description,
+              };
+            }
+          });
+        }
+      } catch (error) {
+        showSnackbarError(error);
+      } finally {
+        setUpdatingRoom(false);
+      }
+    },
+    [rooms, showSnackbarError]
+  );
+
+  return (
+    <RoomsContext.Provider
+      value={{
+        rooms,
+
+        getRooms,
+        loadingRooms,
+        loadingMoreRooms,
+
+        updateRoom,
+        updatingRoom,
+      }}
+    >
+      {children}
+    </RoomsContext.Provider>
+  );
+};
+
+export const useRooms = () => {
+  const store = useContext(RoomsContext);
+  return store;
+};
+
+export default RoomsProvider;
