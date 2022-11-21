8a20b2c0f1a5b59bb3d2921fa838403b4d6762ad
LTCuberik <95138984+LTCuberik@users.noreply.github.com>
2022-11-21 21:20:32 +0700

    Update HomePage.tsx

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -8,6 +8,7 @@ const HomePage = () => {
 
   return (
     <div>
+      <RoomItem data="hello" />
       <RoomItem data={{ id: "SHUBA1", name: "Lớp tập huấn 1", avatar: "https://img6.thuthuatphanmem.vn/uploads/2022/02/13/hinh-anh-lop-hoc-dep-nhat_011959587.jpg" }} />
       <RoomItem data={{ id: "SHUBA2", name: "Lớp tập huấn 2", avatar: "https://img6.thuthuatphanmem.vn/uploads/2022/02/13/hinh-anh-lop-hoc-dep-nhat_011959587.jpg" }} />
       <RoomItem data={{ id: "SHUBA3", name: "Lớp tập huấn 3", avatar: "https://img6.thuthuatphanmem.vn/uploads/2022/02/13/hinh-anh-lop-hoc-dep-nhat_011959587.jpg" }} />
