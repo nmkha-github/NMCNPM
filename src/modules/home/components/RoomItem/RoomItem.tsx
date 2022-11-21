4f5ee9f42c8e809a5a704f93da4ce281443b1235
LTCuberik <95138984+LTCuberik@users.noreply.github.com>
2022-11-21 20:23:58 +0700

    Update RoomItem.tsx

--- src/modules/home/components/RoomItem/RoomItem.tsx
+++ src/modules/home/components/RoomItem/RoomItem.tsx
@@ -1,5 +1,6 @@
 import { Box } from "@mui/material";
 import React from "react";
+import { BiDotsVerticalRounded } from "react-icons/bi";
 
 const RoomItem = ({ data }: { data: any }) => {
   return (
@@ -15,51 +16,50 @@ const RoomItem = ({ data }: { data: any }) => {
         borderWidth: "thin",
         }}
     >
-      <div style = {{
+      <Box style = {{
         width: "100%",
         height: 184,
         display: "block",
         }}>
-        <img src={data.avatar} alt="" style={{
-            width: "100%", 
-            height: "100%",
-            borderRadius: "5% 5% 0 0",
-            }} />
-      </div>
+          <img src={data.avatar} alt="" style={{
+              width: "100%", 
+              height: "100%",
+              borderRadius: "5% 5% 0 0",
+              }} />
+      </Box>
 
-      <div >
-        <div style = {{
+      <Box style = {{
           color: "black",
           padding: "4% 3% 0 4%",
           float: "left",
           }}>
-          <div style ={{
-            fontSize: 16,
-            fontWeight: "bold",
-            overflow: "hidden",
-            cursor: "pointer",
-            }}>
-            {data.name}
-          </div>
+            <Box style ={{
+              fontSize: 16,
+              fontWeight: "bold",
+              overflow: "hidden",
+              cursor: "pointer",
+              }}>
+              {data.name}
+            </Box>
 
-          <div style={{
-            fontSize: 14,
-            paddingTop: "3%"
-            }}>
-            Mã phòng : {data.id}
-          </div>
-        </div>
-      </div>
-      <div>
-          <img src="https://cdn.iconscout.com/icon/free/png-256/more-vertical-1780551-1514427.png" alt="" style={{
+            <Box style={{
+              fontSize: 14,
+              paddingTop: "3%"
+              }}>
+              Mã phòng : {data.id}
+            </Box>
+      </Box>
+      <Box>
+          <BiDotsVerticalRounded style={{
             width: "8%", 
             height: "8%",
             borderRadius: "5% 5% 0 0",
             float: "right",
             cursor: "pointer",
-            padding: "4%"
-            }} />
-          </div>
+            padding: "4%",
+            color: "black"
+            }} id = "demo"/>
+      </Box>
     </Box>
   );
 };
