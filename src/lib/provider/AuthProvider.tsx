82122aef2cdedee6a561758075dc1e5ef7930f07
nmkha-gitlab <nguyenminhkha2001@gmail.com>
2022-11-23 00:14:07 +0700

    update package

--- src/lib/provider/AuthProvider.tsx
+++ src/lib/provider/AuthProvider.tsx
@@ -39,7 +39,7 @@ const AuthProvider = ({ children }: AuthContextProviderProps) => {
     } else {
       console.log(location.pathname);
       if (needAuth) {
-        navigate("/login");
+        //navigate("/login");
       }
     }
   });
