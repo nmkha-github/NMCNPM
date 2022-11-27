8e483a9fe036bb1b0f3148fc94560c5aa6969c98
nmkha-github <nguyenminhkha2001@gmail.com>
2022-11-27 21:54:46 +0700

    fix provider

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import AppSnackbarProvider from './lib/provider/AppSnackBarProvider';
 import { BrowserRouter } from "react-router-dom";
 import AuthProvider from './lib/provider/AuthProvider';
-import AppSnackbarProvider from './lib/provider/AppSnackBarProvider';
 import RoomsProvider from './lib/provider/RoomsProvider';
 import AppRoutes from './AppRoutes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <AppSnackbarProvider>
+    <AppSnackbarProvider>
+      <BrowserRouter>
+        <AuthProvider>
           <RoomsProvider>
             <AppRoutes/>
           </RoomsProvider>
-        </AppSnackbarProvider>
-      </AuthProvider>
-    </BrowserRouter>
+        </AuthProvider>
+      </BrowserRouter>
+    </AppSnackbarProvider>
   </React.StrictMode>
 );
