80872a06860ec017b27e2bb8f3985c956b061346
QuangHuy54 <73436308+QuangHuy54@users.noreply.github.com>
2022-12-16 00:48:04 +0700

    CNPM24-92 Update for missing code.

--- src/pages/RoomPage/[roomId]Page/SettingRoomPage/SettingRoomPage.tsx
+++ src/pages/RoomPage/[roomId]Page/SettingRoomPage/SettingRoomPage.tsx
@@ -1,150 +1,258 @@
-import React, { useCallback, useState } from "react";
-import "./RegisterForm.css";
-import InputRow from "../../../login/components/InputRow/InputRow";
-import EmailHelper from "../../../../lib/util/email-helper";
+import React, { useState, useEffect, useRef } from "react";
+import { Box, Typography, Button, TextField, ButtonBase } from "@mui/material";
+import { useNavigate, useParams } from "react-router-dom";
+import "./SettingRoomPage.css";
+import { Container, Switch } from "@mui/material";
 import useAppSnackbar from "../../../../lib/hook/useAppSnackBar";
-import { useNavigate } from "react-router-dom";
-import { useAuth } from "../../../../lib/provider/AuthProvider";
+import AddIcon from "@mui/icons-material/Add";
+import FileUploadSharpIcon from "@mui/icons-material/FileUploadSharp";
+import ArrowBackSharpIcon from "@mui/icons-material/ArrowBackSharp";
+import RoomData from "../../../../modules/room/interface/room-data";
+import { useRooms } from "../../../../lib/provider/RoomsProvider";
+import { CircularProgress } from "@material-ui/core";
 import LoadingButton from "../../../../lib/components/LoadingButton/LoadingButton";
+import { async } from "@firebase/util";
 
-const RegisterForm = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmpassword, setConfirmPassword] = useState("");
-  const [EmailError, setEmailError] = useState("");
-  const [passwordError, setPasswordError] = useState("");
-  const [confirmPasswordError, setConfirmPasswordError] = useState("");
-  const [registering, setRegistering] = useState(false);
-
-  const { register } = useAuth();
+const SettingRoomPage = () => {
   const navigate = useNavigate();
+  const [roomEditData, setRoomEditData] = useState<RoomData>({
+    id: "",
+    name: "",
+    avatar: "",
+    created_at: "",
+  });
+  const [saving, setSaving] = useState(false);
+  const { roomId } = useParams();
   const { showSnackbarError, showSnackbarSuccess } = useAppSnackbar();
+  const { currentRoom, getCurrentRoom, updateRoom, loadingCurrentRoom } =
+    useRooms();
 
-  const checkRegisterValid = useCallback(() => {
-    if (email === "") {
-      return false;
-    } else if (!EmailHelper.checkEmailValidate(email)) {
-      return false;
+  useEffect(() => {
+    getCurrentRoom(roomId || "");
+  }, []);
+  useEffect(() => {
+    if (currentRoom !== undefined) {
+      setRoomEditData(currentRoom);
     }
-    if (password === "") {
-      return false;
-    }
-    if (confirmpassword === "") {
-      return false;
-    }
-    if (password !== confirmpassword) {
-      return false;
-    }
+  }, [currentRoom]);
 
-    return true;
-  }, [confirmpassword, email, password]);
+  useEffect(() => {
+    console.log(roomEditData);
+  }, [roomEditData]);
 
   return (
-    <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-      <div className="w-full max-w-md space-y-8">
-        <div className="bg-white lg:shadow-2xl rounded px-8 pt-6 pb-8 mb-4 flex flex-col">
-          <div className="mt-8 space-y-6">
-            <h2 className="mt-6 text-left text-3xl font-medium tracking-tight text-gray-900">
-              Đăng ký
-            </h2>
-            <InputRow
-              name="email"
-              id="email"
-              type="text"
-              placeholder="Nhập email để tạo tài khoản..."
-              haveError={EmailError === "" ? false : true}
-              errorText={EmailError}
-              onChange={(event) => {
-                setEmail(event.target.value);
-              }}
-            />
-            <InputRow
-              name="password"
-              id="password"
-              type="Nhập mật khẩu..."
-              placeholder="Password"
-              haveError={passwordError === "" ? false : true}
-              errorText={passwordError}
-              onChange={(event) => {
-                setPassword(event.target.value);
-              }}
-            />
-            <InputRow
-              name="confirmpassword"
-              id="confirmpassword"
-              type="password"
-              placeholder="Xác nhận mật khẩu..."
-              haveError={confirmPasswordError === "" ? false : true}
-              errorText={confirmPasswordError}
-              onChange={(event) => {
-                setConfirmPassword(event.target.value);
-              }}
-            />
+    <Box>
+      <button
+        onClick={() => {
+          navigate("/room/" + roomEditData.id + "/newsfeed");
+        }}
+        className="back_to_workspace"
+      >
+        <ArrowBackSharpIcon /> Back to workspace
+      </button>
+      {loadingCurrentRoom === true ? (
+        <CircularProgress
+          style={{ bottom: "50%", right: "50%", position: "absolute" }}
+          size="60px"
+          color="inherit"
+        />
+      ) : (
+        <Container
+          maxWidth="sm"
+          style={{
+            border: "1px solid",
+            marginTop: "1.25rem",
+            marginBottom: "3rem",
+            paddingBottom: "2.25rem",
+            borderRadius: 8,
+          }}
+        >
+          <h2>Cài đặt</h2>
+          <Box className="setting_row">
+            <Box className="setting_name">
+              <AddIcon fontSize="medium" />
+              <Box className="setting_description">
+                <h3>Tên phòng ban</h3>
+              </Box>
+            </Box>
+          </Box>
 
+          <TextField
+            className="room_name"
+            type="text"
+            onChange={(event) => {
+              setRoomEditData({
+                ...roomEditData,
+                name: event.target.value,
+              });
+            }}
+            fullWidth
+            value={roomEditData == null ? "" : roomEditData.name}
+          />
+          <Box className="setting_row">
+            <Box className="setting_name">
+              <AddIcon fontSize="medium" />
+              <Box className="setting_description">
+                <h3>Mô tả</h3>
+              </Box>
+            </Box>
+          </Box>
+
+          <TextField
+            className="room_name"
+            type="text"
+            onChange={(event) => {
+              setRoomEditData({
+                ...roomEditData,
+                description: event.target.value,
+              });
+            }}
+            fullWidth
+            value={roomEditData == null ? "" : roomEditData.description}
+          />
+          <Box className="setting_row">
+            <Box className="setting_name">
+              <AddIcon fontSize="medium" />
+              <Box className="setting_description">
+                <h3>Khóa phòng ban</h3>
+              </Box>
+            </Box>
+            <Switch
+              checked={roomEditData == null ? false : roomEditData.locked}
+              onChange={(event) => {
+                setRoomEditData({
+                  ...roomEditData,
+                  locked: event.target.checked,
+                });
+              }}
+            />
+          </Box>
+
+          <Box className="setting_row">
+            <Box className="setting_name">
+              <AddIcon fontSize="medium" className="add_icon" />
+              <Box className="setting_description">
+                <h3>Phê duyệt nhân viên</h3>
+              </Box>
+            </Box>
+            <Switch
+              checked={
+                roomEditData == null ? false : roomEditData.auto_accepted
+              }
+              onChange={(event) => {
+                setRoomEditData({
+                  ...roomEditData,
+                  auto_accepted: event.target.checked,
+                });
+              }}
+            />
+          </Box>
+          
+          <h5>
+            Tránh tình trạng những nhân viên không có quyền vào phòng tham gia
+            vào mà không có sự cho phép của quản lý
+          </h5>
+
+          <Box className="setting_row">
+            <Box className="setting_name">
+              <AddIcon fontSize="medium" />
+              <Box className="setting_description">
+                <h3>Tắt hoạt động bảng tin</h3>
+              </Box>
+            </Box>
+            <Switch
+              size="medium"
+              checked={
+                roomEditData == null ? false : roomEditData.disabled_newsfeed
+              }
+              onChange={(event) => {
+                setRoomEditData({
+                  ...roomEditData,
+                  disabled_newsfeed: event.target.checked,
+                });
+              }}
+            />
+          </Box>
+
+          <h5>Tắt mọi hoạt động đăng bài và bình luận</h5>
+
+          <Box className="setting_row">
+            <Box className="setting_name">
+              <AddIcon fontSize="medium" />
+              <Box className="setting_description">
+                <h3>Chặn nhân viên tự ý rời phòng ban</h3>
+              </Box>
+            </Box>
+            <Switch
+              size="medium"
+              checked={roomEditData == null ? false : roomEditData.exit_locked}
+              onChange={(event) => {
+                setRoomEditData({
+                  ...roomEditData,
+                  exit_locked: event.target.checked,
+                });
+              }}
+            />
+          </Box>
+
+          <h5>
+            Giúp dễ dàng quản lý nhân viên tốt hơn, tránh nhân viên tự ý thoát
+            khỏi phòng
+          </h5>
+
+          <Button variant="outlined" className="avatar_change">
+            <img className="image" />
+            <h1 className="change_image">
+              <FileUploadSharpIcon sx={{ fontSize: 40 }} />
+              Chọn file để đổi ảnh
+            </h1>
+            <input type="file" hidden />
+          </Button>
+          <Box
+            className="df"
+            sx={{ display: "flex", justifyContent: "flex-end" }}
+          >
+            <Button variant="outlined" style={{ margin: "0.625rem" }}
+            onClick={()=>{
+              if(currentRoom!==undefined){
+                setRoomEditData(currentRoom);
+              }
+            }}>
+              Hủy thay đổi
+            </Button>
             <LoadingButton
-              fullWidth
-              disabled={!checkRegisterValid() || registering}
-              loading={registering}
               variant="contained"
               color="primary"
-              style={{ borderRadius: 16 }}
-              onClick={async (event) => {
-                if (!checkRegisterValid()) {
-                  if (email === "") {
-                    setEmailError("Email can't be empty.");
-                  } else if (!EmailHelper.checkEmailValidate(email)) {
-                    setEmailError("Invalid email format");
-                  }
-                  if (password === "") {
-                    setPasswordError("Password can't be empty.");
-                  }
-                  if (confirmpassword === "") {
-                    setConfirmPasswordError("Confirm password can't be empty.");
-                  }
-                  if (password !== confirmpassword) {
-                    setConfirmPasswordError(
-                      "Please make sure your password match."
-                    );
-                  }
-                  return;
-                }
-
-                setRegistering(true);
+              disabled={saving}
+              loading={saving}
+              style={{ margin: "0.625rem" }}
+              onClick={async () => {
+                setSaving(true);
                 try {
-                  await register({ email: email, password: password });
-
-                  showSnackbarSuccess("Tạo tài khoản thành công");
-
-                  navigate("/home");
+                  if (roomEditData !== undefined) {
+                    await updateRoom({
+                      id: roomId||"",
+                      updateData: roomEditData,
+                    });
+                    showSnackbarSuccess(
+                      "Cập nhật tùy chỉnh phòng ban thành công."
+                    );
+                    getCurrentRoom(roomId||"");
+                  }
                 } catch (error) {
                   showSnackbarError(error);
                 } finally {
-                  setRegistering(false);
+                  setSaving(false);
                 }
               }}
             >
-              Đăng ký
+              Lưu lại
             </LoadingButton>
-          </div>
-          <div
-            id="seperator_login_register"
-            className="flex my-4 items-center justify-center"
-          >
-            <span className="mx-3">hoặc</span>
-          </div>
-          <div className="flex items-center justify-center">
-            <a
-              href="/login"
-              id="loginButton"
-              className="btn flex w-4/5 justify-center rounded-3xl border-4 border-solid py-2 px-4 text-xl font-bold font-normal text-center"
-            >
-              Quay lại đăng nhập
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
+          </Box>
+        </Container>
+      )}
+    </Box>
   );
 };
 
-export default RegisterForm;
+export default SettingRoomPage;
