c0ed9d7b14fad18b828157761cafb49b6e23956e
QuangHuy54 <73436308+QuangHuy54@users.noreply.github.com>
2022-12-17 11:38:11 +0700

    CNPM24-96 Make task card component

---
+++ src/modules/task/components/TaskCard/TaskCard.tsx
@@ -0,0 +1,130 @@
+import React, { useState, useEffect } from "react";
+import IconButton from "@mui/material/IconButton";
+import { BiDotsVerticalRounded } from "react-icons/bi";
+import { Box, Button, TextField, Container } from "@mui/material";
+import TaskData from "../../../task/interface/task-data";
+import { Typography } from "@material-ui/core";
+import UserHelper from "../../../user/util/user-helper";
+import UserData from "../../../user/interface/user-data";
+import useAppSnackbar from "../../../../lib/hook/useAppSnackBar";
+import USER_AVATAR_DEFAULT from "../../../user/contants/user-avatar-default";
+import { useNavigate, useParams } from "react-router-dom";
+
+interface TaskCardProps {
+  task: TaskData;
+  mode: "card" | "item";
+}
+const TaskCard = ({ task, mode }: TaskCardProps) => {
+  const [taskMenuAnchorEl, setTaskMenuAnchorEl] = useState<null | HTMLElement>(
+    null
+  );
+  const { showSnackbarError } = useAppSnackbar();
+  const navigate = useNavigate();
+  const { roomId } = useParams();
+  const [user, setUser] = useState<undefined | UserData>(undefined);
+  useEffect(() => {
+    async function getUserData() {
+      try {
+        setUser(await UserHelper.getUserById(task.assignee_id));
+      } catch (error) {
+        showSnackbarError(error);
+      }
+    }
+    getUserData();
+  }, []);
+  if (mode === "card") {
+    return (
+      <Box
+        style={{ width: 260, padding: 12, border: "1px solid black" }}
+        sx={{
+          "&:hover": {
+            cursor: "pointer",
+          },
+        }}
+        onClick={() => {
+          navigate(`/room/${roomId}/task/${task.id}`);
+        }}
+      >
+        <Box
+          style={{
+            display: "flex",
+            position: "relative",
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Typography
+            align="center"
+            style={{ textDecoration: "underline", fontSize: 18 }}
+          >
+            {task.title}
+          </Typography>
+          <IconButton
+            style={{ position: "absolute", right: 0 }}
+            onClick={(event: React.MouseEvent<HTMLElement>) =>
+              setTaskMenuAnchorEl(event.currentTarget)
+            }
+          >
+            <BiDotsVerticalRounded />
+          </IconButton>
+        </Box>
+        <Typography style={{ fontSize: 14 }}>{task.content}</Typography>
+        <Box style={{ display: "flex", justifyContent: "right" }}>
+          <Box
+            component="img"
+            sx={{
+              height: 40,
+              width: 40,
+              borderRadius: "50%",
+            }}
+            src={user ? user.avatar : USER_AVATAR_DEFAULT}
+          />
+        </Box>
+      </Box>
+    );
+  } else {
+    return (
+      <Box
+        style={{
+          height: 35,
+          padding: 10,
+          border: "0.1px solid black",
+          display: "flex",
+          alignItems: "center",
+        }}
+        sx={{
+            "&:hover": {
+              cursor: "pointer",
+            },
+          }}
+        onClick={() => {
+          navigate(`/room/${roomId}/task/${task.id}`);
+        }}
+      >
+        <Box
+          component="img"
+          sx={{
+            height: 24,
+            width: 24,
+            borderRadius: "50%",
+          }}
+          src={user ? user.avatar : USER_AVATAR_DEFAULT}
+        />
+        <Typography align="center" style={{ fontSize: 14, marginLeft: 10 }}>
+          {task.title}
+        </Typography>
+        <IconButton
+          style={{ position: "absolute", right: 0 }}
+          onClick={(event: React.MouseEvent<HTMLElement>) =>
+            setTaskMenuAnchorEl(event.currentTarget)
+          }
+        >
+          <BiDotsVerticalRounded />
+        </IconButton>
+      </Box>
+    );
+  }
+};
+
+export default TaskCard;
--- src/modules/user/util/user-helper.ts
+++ src/modules/user/util/user-helper.ts
@@ -1,9 +1,49 @@
 import UserData from "../interface/user-data";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  FieldPath,
+  getDoc,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  startAfter,
+  Timestamp,
+  updateDoc,
+  where,
+} from "firebase/firestore";
+import React, {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { db } from "../../../lib/config/firebase-config";
+import useAppSnackbar from "../../../lib/hook/useAppSnackBar";
 
 const UserHelper = {
   // getUserById: async(id: string): Promise<UserData> => {
   //   return
   // }
+  getUserById:
+    async (id:string):Promise<undefined|UserData> => {
+      try {
+        let result=undefined;  
+        const usersResponse = await getDocs(
+          query(collection(db, "user"), where("id", "==", id))
+        );
+        usersResponse.forEach((userResponse) => {
+          result=userResponse.data();
+        });
+        return result;
+      } catch (error) {
+        console.log(error);
+      }
+    },
 };
 
 export default UserHelper;
