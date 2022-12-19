2c46d4ef5d0d048c28e7b03f45c3264b0143ca20
nmkha-gitlab <nguyenminhkha2001@gmail.com>
2022-12-20 00:17:41 +0700

    CNPM24-112 update assignMemberBox component

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -8,6 +8,9 @@ import AddRoomButton from "../../modules/room/components/AddRoomButton/AddRoomBu
 import RoomItem from "../../modules/room/components/RoomItem/RoomItem";
 import RoomPage from "../RoomPage/RoomPage";
 import UploadFile from "../../lib/components/UploadFile/UploadFile";
+import AssignMemberBox from "../../modules/task/components/AssignMemberBox/AssignMemberBox";
+import MemberData from "../../modules/statistic/interface/member-data";
+import UserData from "../../modules/user/interface/user-data";
 
 const HomePage = () => {
   const dataName = false;
@@ -24,6 +27,17 @@ const HomePage = () => {
 
   return (
     <div>
+      <AssignMemberBox
+        task={{
+          id: "WttA7CdT5Qvbg5XG0xei",
+          title: "",
+          assignee_id: "ujl8YpvL4ogSzkkroLsH",
+          created_at: "",
+          status: "",
+          creator_id: "",
+        }}
+        onChoose={(member) => {}}
+      />
       <UploadFile
         onSuccess={(file) => {
           console.log(file.url);
