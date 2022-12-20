41e0ec8986a05829ca7e56e104bcb48c53f61f5e
nmkha-gitlab <nguyenminhkha2001@gmail.com>
2022-12-20 10:18:51 +0700

    update config

--- src/lib/config/firebase-config.tsx
+++ src/lib/config/firebase-config.tsx
@@ -8,13 +8,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const FirebaseConfig = {
-  apiKey: "AIzaSyASap1ay125TJIPgYsAEqdE4JvXqUhFhT8",
-  authDomain: "nmcnpm-d177c.firebaseapp.com",
-  projectId: "nmcnpm-d177c",
-  storageBucket: "nmcnpm-d177c.appspot.com",
-  messagingSenderId: "658397747139",
-  appId: "1:658397747139:web:38ba98009508d364b0cf76",
-  measurementId: "G-FGC4SP6FC4",
+  apiKey: "AIzaSyDv-o8TSczjA8RiXCpNbuOKS9xgiSKeR9w",
+  authDomain: "nmcnpm-backup.firebaseapp.com",
+  projectId: "nmcnpm-backup",
+  storageBucket: "nmcnpm-backup.appspot.com",
+  messagingSenderId: "904397447943",
+  appId: "1:904397447943:web:774d81784b3d4b4c0bdba2",
+  measurementId: "G-05YQ0V4WBP",
 };
 
 const app = initializeApp(FirebaseConfig);
