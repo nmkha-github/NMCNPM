6954a71ec02e07d1d3e26d460a2a8b8cd2d8881a
nmkha-gitlab <nguyenminhkha2001@gmail.com>
2022-12-30 00:19:48 +0700

    CNPM24-120 update statistic page

--- src/pages/RoomPage/[roomId]Page/StatisticPage/StatisticPage.tsx
+++ src/pages/RoomPage/[roomId]Page/StatisticPage/StatisticPage.tsx
@@ -8,18 +8,11 @@ import { Box, CircularProgress } from "@mui/material";
 import useAppSnackbar from "../../../../lib/hook/useAppSnackBar";
 import BarChart from "../../../../lib/components/BarChart/BarChart";
 
-const MembersPage = () => {
+const StatisticPage = () => {
   const { roomId } = useParams();
   const { user } = useUser();
-  const { currentRoom, getCurrentRoom } = useRooms();
-  const {
-    members,
-    getMembers,
-    loadingMembers,
-    roomStatistic,
-    getRoomStatistic,
-    loadingRoomStatistic,
-  } = useStatistic();
+  const { currentRoom, getCurrentRoom, loadingCurrentRoom } = useRooms();
+  const { roomStatistic, getRoomStatistic } = useStatistic();
 
   const { showSnackbarError } = useAppSnackbar();
   const navigate = useNavigate();
@@ -36,37 +29,42 @@ const MembersPage = () => {
         navigate(`/room`);
         return;
       }
-      getMembers({ room_id: currentRoom.id });
       getRoomStatistic({ room_id: currentRoom.id });
     }
   }, [currentRoom]);
 
   return (
     <LeftSideBar>
-      <Box
-        style={{
-          height: 400,
-          marginTop: 24,
-          position: "relative",
-        }}
-      >
-        <BarChart
+      {loadingCurrentRoom ? (
+        <Box style={{ marginTop: 16 }}>
+          <CircularProgress />
+        </Box>
+      ) : (
+        <Box
           style={{
             height: 400,
-            width: "100%",
-            position: "absolute",
-            bottom: 0,
+            marginTop: 24,
+            position: "relative",
           }}
-          data={[
-            { value: roomStatistic.toDo, title: "Chưa làm" },
-            { value: roomStatistic.doing, title: "Đang làm" },
-            { value: roomStatistic.reviewing, title: "Chờ duyệt" },
-            { value: roomStatistic.done, title: "Hoàn thành" },
-          ]}
-        />
-      </Box>
+        >
+          <BarChart
+            style={{
+              height: 400,
+              width: "100%",
+              position: "absolute",
+              bottom: 0,
+            }}
+            data={[
+              { value: roomStatistic.toDo, title: "Chưa làm" },
+              { value: roomStatistic.doing, title: "Đang làm" },
+              { value: roomStatistic.reviewing, title: "Chờ duyệt" },
+              { value: roomStatistic.done, title: "Hoàn thành" },
+            ]}
+          />
+        </Box>
+      )}
     </LeftSideBar>
   );
 };
 
-export default MembersPage;
+export default StatisticPage;
