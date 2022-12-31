8a38086d1764e64103bad772e4f8052b3a6f3641
LTCuberik <95138984+LTCuberik@users.noreply.github.com>
2022-12-31 10:07:00 +0700

    Update firebase-config.tsx

--- src/lib/config/firebase-config.tsx
+++ src/lib/config/firebase-config.tsx
@@ -12,24 +12,24 @@ import { getFirestore } from "firebase/firestore";
 const FirebaseConfig = {
   apiKey: "AIzaSyASap1ay125TJIPgYsAEqdE4JvXqUhFhT8",
   authDomain: "nmcnpm-d177c.firebaseapp.com",
-  databaseurl: "https://nmcnpm-d177c-default-rtdb.firebaseio.com",
+  databaseURL: "https://nmcnpm-d177c-default-rtdb.firebaseio.com",
   projectId: "nmcnpm-d177c",
   storageBucket: "nmcnpm-d177c.appspot.com",
-  messagingSenderid: "658397747139",
+  messagingSenderId: "658397747139",
   appId: "1:658397747139:web:38ba98009508d364b0cf76",
   measurementId: "G-FGC4SP6FC4",
 };
-/*
+
 //Backup firebase
-const FirebaseConfig = {
-  apiKey: "AIzaSyDv-o8TSczjA8RiXCpNbuOKS9xgiSKeR9w",
-  authDomain: "nmcnpm-backup.firebaseapp.com",
-  projectId: "nmcnpm-backup",
-  storageBucket: "nmcnpm-backup.appspot.com",
-  messagingSenderId: "904397447943",
-  appId: "1:904397447943:web:774d81784b3d4b4c0bdba2",
-  measurementId: "G-05YQ0V4WBP",
-};*/
+// const FirebaseConfig = {
+//   apiKey: "AIzaSyDv-o8TSczjA8RiXCpNbuOKS9xgiSKeR9w",
+//   authDomain: "nmcnpm-backup.firebaseapp.com",
+//   projectId: "nmcnpm-backup",
+//   storageBucket: "nmcnpm-backup.appspot.com",
+//   messagingSenderId: "904397447943",
+//   appId: "1:904397447943:web:774d81784b3d4b4c0bdba2",
+//   measurementId: "G-05YQ0V4WBP",
+// };
 
 const app = initializeApp(FirebaseConfig);
 export const db = getFirestore(app);
