7f899f5bbbd3790afc293b57e9222816d3a0850f
LTCuberik <95138984+LTCuberik@users.noreply.github.com>
2023-01-08 10:26:00 +0700

    Update RegisterPage.tsx

--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -9,7 +9,7 @@ const RegisterPage = () => {
       paddingTop: "5%" ,
       background: "url(https://firebasestorage.googleapis.com/v0/b/nmcnpm-d177c.appspot.com/o/files%2FBG_LOGIN_Page.png?alt=media&token=7aefc7b1-9b15-43d0-9d75-650eb5060c98)"
     }}>
-      <RegisterForm />
+      <RegisterForm/>
     </Box>
    </Box>
 };
